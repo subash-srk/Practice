eae318c491a81667f58427bc0956ed2c2a43c643
Subash K <81848034+subash-srk@users.noreply.github.com>
2024-02-28 19:45:03 +0530

    Add files via upload

---
+++ c++/basic.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //basic
+    cout<<"Hello World!" << endl << "Hello" <<endl;
+    /*
+
+    //Arithmetic
+    cout << "Enter number for a: ";
+    double a,b;
+    cin>> a;
+    cout << "Enter number for b: ";
+    cin >> b;
+
+    cout << "a = " << a <<endl;
+    cout << "b = " << b <<endl;
+    cout << "a + b = " << a + b <<endl;
+    cout << a << " + " << b << " = " << a+b <<endl;
+    int sub = a-b;
+    double div = a/b;
+    cout << "div = " << div <<endl;
+    */
+    //datatypes
+
+    int age = 21;
+    double price = 99.99;
+    float interestRate = 3.67f;
+    long fileSize = 90000L;
+    char letter = 'a';
+    string name = "Akira";
+    bool isValid = true;
+    auto years = 11;
+
+    //Number Sytems
+    int x = 255;
+    int y = 0b0011111; //Binary
+    int z = 0xFF; //HexaDecimal
+    //cout << z;
+
+    //Random Numbers
+
+}
+
